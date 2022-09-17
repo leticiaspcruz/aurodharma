55c27edd5561575735f53bcaa0694ffa5575b4ca
leticiaspcruz <leticiaspcruz@gmail.com>
2022-09-17 17:39:48 -0300

    new contact component

--- src/components/contact/contact-modal.tsx
+++ src/components/contact/contact-modal.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {Container} from './style';
+import {Container, Title, ContactButton} from './style';
 
 const ContactModal = () => {
   return (
     <>
-      <Container>
-        Teste
+      <Container id="contact">
+        <Title>fale com a banda!</Title>
+        <ContactButton onClick={()=> {window.open(
+            "https://api.whatsapp.com/send?phone=5511983551895"
+            )}}>WhatsApp</ContactButton>
+        <ContactButton onClick={()=> {window.open(
+            "mailto:aurodharma@gmail.com"
+            )}}>E-mail</ContactButton>
       </Container>
     </>
   )
--- src/components/contact/style.ts
+++ src/components/contact/style.ts
@@ -2,7 +2,26 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  flex-direction:row;
-  height: 40%;
-  margin-top: 20px;
+  flex-direction: column;
+  align-items: center;
+  background: black;
+  font-size: 30px;
+  padding: 50px;
+  text-align: center;
+`;
+
+export const ContactButton = styled.button`
+  background: #D1AB24;
+  width: 30%;
+  padding: 30px;
+  margin: 10px 0;
+  font-family: 'Poppins', sans-serif;
+  font-size: 22px;
+`;
+
+export const Title = styled.h1`
+  color: #D1AB24;
+  font-family: 'Poppins', sans-serif;
+  font-weight: bold;
+  font-size: 30px;
 `;
